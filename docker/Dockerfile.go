75f1c43f9663a80a50efe0124397de04842ca7af
Benedict Homuth <bhomuth@inovex.de>
2023-03-28 12:13:50 +0200

    fixed workflow + simplified vm setup for container runtime

--- docker/Dockerfile.go
+++ docker/Dockerfile.go
@@ -1,5 +1,4 @@
 FROM golang:1.20.2 as builder
-# FROM golang:alpine3.15 as builder
 WORKDIR /iot-app
 COPY go.mod .
 COPY go.sum .
@@ -12,7 +11,7 @@ ENV CGO_ENABLED=0
 RUN go build -o backend-server main.go && chmod +x ./backend-server
 
 FROM scratch
-# LABEL org.opencontainers.image.source https://github.com/benedicthomuth/bachelor-thesis
+LABEL org.opencontainers.image.source https://github.com/benedicthomuth/iot4Dobot
 COPY /frontend /frontend
 COPY --from=builder /iot-app/backend-server backend-server
 ENTRYPOINT [ "./backend-server" ]
