dde1a739836375a2a40a034f924efe2a975c68a7
Benedict Homuth <bhomuth@inovex.de>
2023-03-28 22:06:24 +0200

    exposing all containers on local server host

--- terraform/var.tf
+++ terraform/var.tf
@@ -10,8 +10,8 @@ variable "podman_setup" {
         curl \
         gnupg
     sudo apt-get -y install podman
-    podman run -p 4222:4222 docker.io/library/nats:2.9.15
-    podman run -p 8080:8080 -e NATS_CONN_STRING="localhost:4222" ghcr.io/benedicthomuth/iot4dobot/server:latest
+    podman run -p 4222:4222 --name=nats --network=host -d docker.io/library/nats:2.9.15
+    podman run -p 8080:8080 --name=backend --network=host -d -e NATS_CONN_STRING="localhost:4222" ghcr.io/benedicthomuth/iot4dobot/server:latest
   EOF
   description = "Sets up podman and start NATS + Backend"
 }
