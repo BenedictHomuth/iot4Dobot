579119c4e241c36d30c84840d1b443237e01ac38
Benedict Homuth <bhomuth@inovex.de>
2023-03-31 11:32:10 +0200

    restructured the repo for more clarity

--- publisher/test-data-stream-main.go
+++ publisher/test-data-stream-main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"time"
 
@@ -14,20 +15,24 @@ type PubMessage struct {
 }
 
 type RoboEvent struct {
-	X      string       `json:"x"`
-	Y      string       `json:"y"`
-	Z      string       `json:"z"`
-	R      string       `json:"r"`
+	X      float64      `json:"x"`
+	Y      float64      `json:"y"`
+	Z      float64      `json:"z"`
+	R      float64      `json:"r"`
 	Angles JointsAngles `json:"jointAngle"`
 }
 
 type JointsAngles struct {
-	Rotations [4]string
+	Rotations [4]float64
+}
+
+func randFloats(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
 }
 
 func main() {
 	// nc, err := nats.Connect(nats.DefaultURL)
-	nc, err := nats.Connect("65.109.172.100:4222")
+	nc, err := nats.Connect("159.69.125.114:4222")
 	if err != nil {
 		fmt.Println("Oh no. NATS server not found")
 	}
@@ -37,14 +42,14 @@ func main() {
 	var msg PubMessage
 	var armPos RoboEvent
 	for {
-		armPos.X = strconv.Itoa(index)
-		armPos.Y = strconv.Itoa(index + 1)
-		armPos.Z = strconv.Itoa(index + 2)
-		armPos.R = strconv.Itoa(index + 3)
-		armPos.Angles.Rotations[0] = "Test #1"
-		armPos.Angles.Rotations[1] = "Test #2"
-		armPos.Angles.Rotations[2] = "Test #3"
-		armPos.Angles.Rotations[3] = "Test #4"
+		armPos.X = 0.0
+		armPos.Y = 0.0
+		armPos.Z = randFloats(0.0, 400.0)
+		armPos.R = 0.0
+		armPos.Angles.Rotations[0] = 0.0
+		armPos.Angles.Rotations[1] = randFloats(0.0, 90.0)
+		armPos.Angles.Rotations[2] = 0.0
+		armPos.Angles.Rotations[3] = randFloats(0, 45.0)
 
 		msg.Data = strconv.Itoa(index)
 		jmsg, err := json.Marshal(armPos)
@@ -52,7 +57,7 @@ func main() {
 			fmt.Println("Could not marshal message to publish!")
 		}
 		nc.Publish("roboPos", []byte(jmsg))
-		fmt.Println("published: " + msg.Data)
+		fmt.Println("published: " + fmt.Sprintf("%f", armPos))
 		time.Sleep(500 * time.Millisecond)
 		index++
 	}
